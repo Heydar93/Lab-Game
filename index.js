56f028e45ed3b7b56c46d749c7023d49f0e885fb
Valiyev Heydar <HeydarnV@code.local>
2023-06-16 19:50:41 +0400

    complete

---
+++ index.js
@@ -0,0 +1,31 @@
+function Game() {
+    let choices = ["rock", "paper", "scissors"];
+    let player = prompt("Choose rock, paper, or scissors:");
+    let computer = choices[Math.floor(Math.random() * 3)];
+  
+    let result;
+    if (player === computer) {
+      result = "Try again!";
+    } else if (
+      (player === "rock" && computer === "scissors") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissors" && computer === "paper")
+    ) {
+      result = "You win!";
+    } else {
+      result = "Computer wins!";
+    }
+  
+    alert(
+      "Player's choice: " +
+        player + 
+        "Computer's choice: " +
+        computer +
+        "Result: " +
+        result
+    );
+  }
+
+  Game(); 
+  
+  
